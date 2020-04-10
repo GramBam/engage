84f6902ef925767b7df0070d43a6b53b0b478d82
Graham <graham.moss@hotmail.com>
2020-04-10 16:54:16 -0400

    first commit, add particle lib

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+
+const params = {
+  "particles": {
+    "number": {
+      "value": 69
+    },
+    "size": {
+      "value": 3
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="home">
+      <p className="title">ENGAGE PRODUCTIONS</p>
+      <Particles params={params}/>
     </div>
   );
 }
