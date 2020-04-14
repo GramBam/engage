400fb2a6bd7d2c515ab0525718c79119b0f4e6a0
Graham <graham.moss@hotmail.com>
2020-04-14 00:53:48 -0400

    responsive functionality to event card component, about component

--- src/EventCard.js
+++ src/EventCard.js
@@ -5,12 +5,12 @@ function EventCard() {
   return (
     <div className="card">
       <img className="card__img" src="https://i.imgur.com/Rb8Kh2Y.jpg" alt="" />
-      <div style={{width: "20vw"}}>
+      <div className="card__info-wrap">
         <p className="card__title">Rave Against Time</p>
-        <ul className="card__info">
-          <li>Saturday, November 4 - 10:00pm</li>
-          <li>Maezo Indian Bar</li>
-        </ul>
+        <div className="card__info">
+          <p>Saturday, November 4 - 10:30pm</p>
+          <p>Maezo Indian Bar</p>
+        </div>
       </div>
     </div>
   )
