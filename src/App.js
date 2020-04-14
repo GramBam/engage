84666cb32e2fd55f27f6dcc5a17bd906091c09ad
Graham <graham.moss@hotmail.com>
2020-04-13 21:10:14 -0400

    working command line

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 import Nav from './Nav';
@@ -8,16 +8,33 @@ import {
 } from "react-router-dom";
 import Events from './Events';
 import About from './About';
+import CommandLine from './CommandLine';
 
 function App() {
+  const [commandHidden, setCommandHidden] = useState(true);
+
+  React.useEffect(() => {
+    const onKey = (e) => {
+      console.log(e);
+      if(e.ctrlKey && e.key === '`') {
+        setCommandHidden(value => !value);
+      }
+    }
+
+    document.addEventListener('keydown', onKey);
+
+    return () => document.removeEventListener('keydown', onKey);
+  }, [])
+
   return (
     <div className="home">
       <p className="title">ENGAGE PRODUCTIONS</p>
       <Nav />
-        <Switch>
-          <Route path="/events" component={Events} />
-          <Route path="/about" component={About} />
-        </Switch>
+      <Switch>
+        <Route path="/events" component={Events} />
+        <Route path="/about" component={About} />
+      </Switch>
+      <CommandLine hidden={commandHidden}/>
       <Particles params={params} />
     </div>
   );
---
+++ src/CommandLine.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './CommandLine.css';
+
+function CommandLine(props) {
+  const [inputValue, updateInput] = useState('');
+
+  const onChange = (e) => {
+    updateInput(e.target.value.toLowerCase());
+  }
+
+  const enterCheck = (e) => {
+    if(e.key === 'Enter') {
+      let url;
+      switch(inputValue) {
+        case 'kisa': url = 'https://www.youtube.com/watch?v=0vt47I3k088'; break;
+        case 'totw': url = 'https://www.youtube.com/watch?v=5V-auvEuLO4'; break;
+        case 'wcbh': url = 'https://youtu.be/360qEjSpGdI?t=4'; break;
+        default: return;
+      }
+      if(url) {
+        window.open(url);
+      }
+    }
+  }
+
+  return (
+    <div className="command-line" style={{display: props.hidden ? 'none' : 'flex'}}>
+      <p className="command-line__text">></p>
+      <input className="command-line__input" onChange={onChange} onKeyPress={enterCheck} autoFocus />
+    </div>
+  )
+}
+
+export default CommandLine;
